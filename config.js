afd19b3197081cb2a35fb6e07aa3d370a220d01e
username23487 <egemengokturkokul@gmail.com>
2025-10-11 17:36:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
 // config.js
 
-// BU BİLGİLERİ KENDİ FIREBASE PROJENDEN ALIP DOLDUR
 const firebaseConfig = { 
-    apiKey: "SENİN-FIREBASE-API-KEYİN-BURAYA", 
-    authDomain: "SENİN-AUTH-DOMAININ-BURAYA", 
-    databaseURL: "SENİN-DATABASE-URLN-BURAYA", 
-    projectId: "SENİN-PROJE-IDN-BURAYA", 
-    messagingSenderId: "SENİN-SENDER-IDN-BURAYA", 
-    appId: "SENİN-APP-IDN-BURAYA"
+    apiKey: "AIzaSyC5XS00Wu02kEVYj5thRuDXl8zhLQrRDR4", 
+    authDomain: "chatuygulama-cbf18.firebaseapp.com", 
+    databaseURL: "https://chatuygulama-cbf18-default-rtdb.firebaseio.com", 
+    projectId: "chatuygulama-cbf18", 
+    messagingSenderId: "881374423023", 
+    appId: "1:881374423023:web:8b17a88119d9123295381a"
 };
 
-// BU BİLGİYİ KENDİ IMGBB HESABINDAN ALIP DOLDUR
-const IMGBB_API_KEY = "SENİN-IMGBB-API-KEYİN-BURAYA";
+const IMGBB_API_KEY = "623d06e4c4f8135062c33f2e4e3cddd9";
